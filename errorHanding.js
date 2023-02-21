75ac372df819bce05c6021ca98bd03d2240d06ad
AMARNATH833 <amarnath0862>
2023-02-21 17:19:13 +0530

    ExpressJS

---
+++ errorHanding.js
@@ -0,0 +1,17 @@
+//Example of Error Handing in Express JS.....
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   var err = new Error("Something went wrong");
+   next(err);
+});
+
+//An error handling middleware
+app.use(function(err, req, res, next) {
+   res.status(500);
+   res.send("Oops, something went wrong.")
+});
+
+app.listen(3000);
